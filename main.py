afe5ea8ed509106c3e7fbadfe7c888645983cfd0
Akshat Kumar <akshatkumar.2001@gmail.com>
2023-07-30 11:08:55 +0530

    Commits

--- main.py
+++ main.py
@@ -5,12 +5,12 @@ import os
 from typing import List
 
 # Load your OpenAI API key
-models.OpenAI.api_key = "YOUR_API_KEY"
+models.OpenAI.api_key = "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"
 # or from environment variable:
 # models.OpenAI.api_key = os.getenv("OPENAI_API_KEY")
 
 # Prompt for GPT-3.5 Turbo
-SYSTEM_PROMPT = """You are chatting with an AI. There are no specific prefixes for responses, so you can ask or talk about anything you like. The AI will respond in a natural, conversational manner. Feel free to start the conversation with any question or topic, and let's have a pleasant chat!
+SYSTEM_PROMPT = """Please act as a mental health adviser. I will provide you with a patient looking for guidance and advice on managing their stress, anxiety and other mental health issues. You should use your knowledge of all mental issues and their respective solutions in order to create strategies that the individual can implement for overall improvement. Your tone should be precise, soothing and easy to understand. My first response is "I need your help to cope up with my depression symptoms".
 """
 
 
@@ -31,7 +31,7 @@ def on_message(message_history: List[Message], state: dict = None):
     # # Generate GPT-3.5 Turbo response
     bot_response = models.OpenAI.generate(
         system_prompt=SYSTEM_PROMPT,
-        message_history=message_history,
+        message_history=message_history[-2:],
         model="gpt-3.5-turbo",
     )
 
